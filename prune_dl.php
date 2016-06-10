c6e28e9327540a8c0f2c108510dbdda6ff0ae6c1
rg-viza <rg.viza@gmail.com>
2016-06-09 23:52:16 -0400

    Update prune_dl.php
    ok much better

--- prune_dl.php
+++ prune_dl.php
@@ -2,17 +2,10 @@
 <?php
 /** set $tr to the full path of transmission-remote plus a space **/
 $tr = '/usr/local/bin/transmission-remote ';
-exec($tr.'--list', $arrTorrents);
+exec($tr.'--list | grep 100\%', $arrTorrents);
 foreach($arrTorrents as $idx=>$torrent)
 {
-        if($idx>0)
-        {
-                $pctcomplete = substr($torrent,7,3);
-                if($pctcomplete == '100')
-                {
-                        $tid = str_replace(" ", "", substr($torrent, 0, 4));
-                        exec($tr."-t $tid -r");
-                }
-        }
+        $tid = str_replace(" ", "", substr($torrent, 0, 4));
+        exec($tr."-t $tid -r");
 }
 ?>
