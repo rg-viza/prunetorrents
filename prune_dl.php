2d0c5838c8af61495df902e3cba1bcd4018e5d02
rg-viza <rg.viza@gmail.com>
2016-06-09 23:24:47 -0400

    Update prune_dl.php

--- prune_dl.php
+++ prune_dl.php
@@ -1,6 +1,5 @@
 #!/bin/php
 <?php
-/** Set $tr to the transmission-remote path. Add a space to the end of the string **/
 $tr = '/usr/local/bin/transmission-remote ';
 exec($tr.'--list', $arrTorrents);
 foreach($arrTorrents as $idx=>$torrent)
@@ -11,8 +10,7 @@ foreach($arrTorrents as $idx=>$torrent)
                 if($pctcomplete == '100')
                 {
                         $tid = str_replace(" ", "", substr($torrent, 0, 4));
-                        $command = $tr."-t $tid -r";
-                        exec($command); 
+                        exec($tr."-t $tid -r");
                 }
         }
 }
